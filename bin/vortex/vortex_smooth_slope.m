8e9dc81ed1e3fbc6d9d6b2ebdc5a96de8ef81c39
a.w.baggaley <a.w.baggaley@47f1faa4-cbe0-e64c-5756-cffd268ae9a8>
2010-11-02 12:41:18 +0000

    improved the following matlab routines

--- bin/vortex/vortex_smooth_slope.m
+++ bin/vortex/vortex_smooth_slope.m
@@ -1,12 +1,22 @@
-function vortex_smooth_slope
+%a function which loops over all available var files and computes the slope of the 
+%ampitude spectrum, then plots this as a function of time.
+function vortex_smooth_slope(skip)
   ts=load('./data/ts.log');
   t=ts(:,2);
   s=length(t);
-  for i=1:s
+  for i=1:skip:s
+      if skip<10
+        if mod(i,10)==0
+          disp(sprintf('processing varfile %04d of %04d',i,s))
+        end
+      else
+        disp(sprintf('processing varfile %04d of %04d',i,s))
+      end
       p=vortex_smooth(i,10,'noplot');
       slope(i)=p(1);
   end
-  plot(t,slope,'k','LineWidth',2)
+  plot(t(1:skip:s),slope(1:skip:s),'k','LineWidth',2)
   set(gca,'FontSize',14)
   xlabel('t','FontSize',14)
   ylabel('slope','FontSize',14)
+end
