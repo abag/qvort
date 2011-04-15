e62b5e9c87987d8d809bdae0331b7a9fb40089f9
Andrew Baggaley <a.w.baggaley@gmail.com>
2011-04-15 15:57:23 +0200

    improved this routine to produce more publication style plots

--- bin/vortex/line_length_fit.m
+++ bin/vortex/line_length_fit.m
@@ -1,6 +1,5 @@
-%read in the ts file and plot the line length in both normal and logscale
-%takes an input that is the number of sections to divide the plot into and give
-%a growth/decay rate
+%read in the ts file and plot the line length in logscale
+%takes an input that is a line to fit to log-log plot
 function line_length(fit)
 if nargin<1
   fit=-5/3
@@ -8,6 +7,14 @@ end
 A=load('data/ts.log');
 t=A(:,2) ; l=A(:,6) ; 
 fitted_line=t.^(fit) ;
-factor=sum(l(200:length(l)))/sum(fitted_line(200:length(l)))
-fitted_line=fitted_line*factor
-loglog(t,l,t,fitted_line)
+%get the fitted line to lie over the data
+factor=sum(l(floor(0.75*length(l)):length(l)))/sum(fitted_line(floor(0.75*length(l)):length(l)));
+fitted_line=1.5*fitted_line*factor;
+loglog(t,l,'k*','MarkerSize',4)
+hold on
+loglog(t,fitted_line,'r-','LineWidth',2)
+hold off
+set(gca,'FontSize',14)
+xlabel('log t','FontSize',14)
+ylabel('log L','FontSize',14)
+text(sum((t))./size(t),sum((fitted_line))./size(t),num2str(fit),'FontSize',14)
