e43ce343abbba583ad077035218edeeda9b340c7
Andrew Baggaley <a.w.baggaley@gmail.com>
2011-06-14 09:07:00 +0100

    added a new routine to plot histograms of vortex point separations

---
+++ bin/vortex/vortex_sep_hist.m
@@ -0,0 +1,74 @@
+function vortex_sep_hist(filenumber)
+filename=sprintf('data/var%04d.log',filenumber);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%get the dimensions information from dims.log
+dims=load('./data/dims.log');
+if dims(4)==1
+  fid=fopen(filename);
+  if fid<0
+      disp('var file does not exist, exiting script')
+      return
+  end
+  time=fread(fid,1,'float64');
+  number_of_particles=fread(fid,1,'int');
+  x=fread(fid,number_of_particles,'float64');
+  y=fread(fid,number_of_particles,'float64');
+  z=fread(fid,number_of_particles,'float64');
+  f=fread(fid,number_of_particles,'int');
+  u=fread(fid,number_of_particles,'float64');
+else 
+  fid=fopen(filename);
+  if fid<0
+      disp('var file does not exist, exiting script')
+      return
+  end
+  %read the time
+  tline=fgetl(fid);
+  dummy=textscan(tline, '%f');
+  time=dummy{:};
+  %how many particles
+  tline=fgetl(fid);
+  dummy=textscan(tline, '%d');  
+  number_of_particles=dummy{:};
+  %get the particles%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  for j=1:number_of_particles
+    tline=fgetl(fid);
+    dummy=textscan(tline, '%f');
+    dummy_vect=dummy{:};
+    x(j)=dummy_vect(1);
+    y(j)=dummy_vect(2);
+    z(j)=dummy_vect(3);
+    f(j)=dummy_vect(4);
+    u(j)=dummy_vect(5);
+  end
+  f=uint16(f);
+end
+%now create vectors to plot%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+counter=1;
+for j=1:number_of_particles
+  if round(f(j))==0
+  else
+    dummy_x(1,1)=x(j);
+    dummy_x(2,1)=x(round(f(j)));
+    dummy_x(1,2)=y(j);
+    dummy_x(2,2)=y(round(f(j)));
+    dummy_x(1,3)=z(j);
+    dummy_x(2,3)=z(round(f(j)));
+    dist=sqrt((dummy_x(1,1)-dummy_x(2,1))^2+(dummy_x(1,2)-dummy_x(2,2))^2+(dummy_x(1,3)-dummy_x(2,3))^2);
+    if (dist<4*dims(1))
+      sep(counter)=dist;
+      counter=counter+1;
+    end
+  end
+end
+figure('Name','Histogram of particle separation')
+ hist(sep)
+ xlabel('separation','FontSize',16)
+ ylabel('N','FontSize',16)
+ set(gca,'FontSize',16)
+figure('Name','PDF particle separation')
+ [f xi]=ksdensity(sep)
+ plot(xi,f,'-k','LineWidth',2)
+ xlabel('separation','FontSize',16)
+ ylabel('PDF','FontSize',16)
+ set(gca,'FontSize',16)
