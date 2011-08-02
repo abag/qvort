ee73050647b60064ba4a1aef73b1b5fc3c0095e3
Andrew Baggaley <a.w.baggaley@gmail.com>
2011-08-02 15:04:44 +0100

    restored normal fluid scripts which must have been deleted accidentaly!

---
+++ bin/normal_fluid/norm_init.m
@@ -0,0 +1,29 @@
+function norm_init
+close all
+filename='data/norm_init_mesh.dat';
+load data/nfm_dims.log;
+msize=nfm_dims(1)
+if (msize==0) 
+  disp('mesh size is zero exiting script')
+  return
+end
+fid=fopen(filename);
+if fid<0
+  disp('mesh file does not exist, exiting script')
+  return
+end
+x=fread(fid,msize,'float64');
+ux=fread(fid,msize^3,'float64');
+uy=fread(fid,msize^3,'float64');
+uz=fread(fid,msize^3,'float64');
+fclose(fid)
+ux=reshape(ux,msize,msize,msize);
+uy=reshape(uy,msize,msize,msize);
+uz=reshape(uz,msize,msize,msize);
+unorm=max(sqrt(ux.^2+uy.^2+uz.^2));
+figure('Name','u(x)')
+  plot(squeeze(ux(32,32,:)))
+%plot slices of field+isosurface
+mesh_slices(x,ux,uy,uz,msize,'initial normal fluid')
+figure('Name','quiver')
+ quiver(ux,uy,uz);  
