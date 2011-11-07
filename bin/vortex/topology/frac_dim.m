f458a5cebcc17c1a230fe926fbbc4ca58dfde670
Andrew Baggaley <nawb4@dreher.(none)>
2011-11-07 20:09:48 +0000

    changes to fractal dimension routine

--- bin/vortex/topology/frac_dim.m
+++ bin/vortex/topology/frac_dim.m
@@ -1,8 +1,13 @@
-%a function which loops over all available var files and computes the correlation
+%a function which loops over all available var files and computes the fractal
 %dimension of the vortex filaments, then plots this as a function of time.
-function coor_dim_all(start,finish,skip)
-  if nargin==0
-    skip=1;
+%takes the option method which should be set to haus or corr
+function coor_dim_all(start,finish,skip,method)
+  if nargin<3
+    disp('need thre input arguments start,finish,skip')
+    return
+  end 
+  if nargin==3
+    method='haus';
   end 
   ts=load('./data/ts.log');
   t=ts(:,2);
@@ -15,8 +20,14 @@ function coor_dim_all(start,finish,skip)
       else
         disp(sprintf('processing varfile %04d of %04d',i,s))
       end
-      p=corr_dim(i,'noplot');
-      slope(i)=p(1);
+      switch method
+        case 'haus'
+      p=haus_dim(i);
+      slope(i)=p;
+        case 'corr'
+        p=corr_dim(i,'noplot');
+        slope(i)=p(1);
+      end
   end
   plot(t(start:skip:finish),slope(start:skip:finish),'k','LineWidth',2)
   set(gca,'FontSize',14)
