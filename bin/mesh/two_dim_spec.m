34040ef5534f1038852a36e7a6faf94de58e6950
A.W. Baggaley <nawb4@hardy.ncl.ac.uk>
2011-11-15 22:28:33 +0000

    added a new routine to compute average 2D specrta and also added colour to two_dim_spec

--- bin/mesh/two_dim_spec.m
+++ bin/mesh/two_dim_spec.m
@@ -7,6 +7,8 @@ elseif nargin<3
 else
   do_fit=1;
 end
+cmap=colormap(jet(length(filenumbers)));
+color_count=1;
 for i=filenumbers
 filename=sprintf('data/vel_slice_2D%04d.dat',i);
 fid=fopen(filename);
@@ -14,6 +16,7 @@ if fid<0
   disp('2D slice file does not exist, exiting script')
   return
 end
+dims=load('./data/dims.log');
 A=fread(fid,'float64');
 s=length(A); s=s/8; s=sqrt(s);
 B=reshape(A,8,s,s);
@@ -46,20 +49,24 @@ for i=1:n
     spect(r)=spect(r)+energyr(i,j)+energyi(i,j);
   end
 end
-k=1:midpt;
-k2=(floor(midpt/2):midpt);
-loglog(k(1:midpt-cutoff),spect(1:midpt-cutoff),'k','LineWidth',2)
+k=(1:midpt)*(2*pi/dims(2));
+if length(filenumbers)>1
+  loglog(k(1:midpt-cutoff),spect(1:midpt-cutoff),'Color',cmap(color_count,:),'LineWidth',1.5)
+else
+  loglog(k(1:midpt-cutoff),spect(1:midpt-cutoff),'Color','k','LineWidth',1.5)
+end
+color_count=color_count+1;
 hold on
 end
 %%%%%%%%%%%%%%%%%APPLY FIT%%%%%%%%%%%%%%%%%
 if do_fit==1
   disp(sprintf('fitting a slope of %f to plot',fit))
   dummy_spect=k.^(fit);
-  scaling_factor=sum(spect(1:10))/sum(dummy_spect(1:10));
+  scaling_factor=0.8*sum(spect(1:10))/sum(dummy_spect(1:10));
   dummy_spect=dummy_spect*scaling_factor;
   hold on
   loglog(k,dummy_spect,'--r','LineWidth',2)
 end 
 %%%%%%%%%%%%ANNOTATE%%%%%%%%%%%%%%%%%%%%%%
-xlabel('log k','FontSize',14) ; ylabel('log E(k)','FontSize',14)
-set(gca,'FontSize',14)
+xlabel('log k','FontSize',16) ; ylabel('log E(k)','FontSize',16)
+set(gca,'FontSize',16)
