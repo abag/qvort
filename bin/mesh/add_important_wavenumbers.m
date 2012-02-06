2506b44e6402b6bef9c16fec14fb2fc0666570c6
Sherwin LK <a7124457@hardy.ncl.ac.uk>
2012-02-06 11:17:56 +0000

    changed colours, moved to lower resolution to display resolution cutoff

--- bin/mesh/add_important_wavenumbers.m
+++ bin/mesh/add_important_wavenumbers.m
@@ -5,14 +5,14 @@ dumy(1)=range(1);
 dumy(2)=range(2);
 dumx(1:2)=k_intervortex;
 hold on
-plot(dumx,dumy,'k--')
+plot(dumx,dumy,'b--')
 dims=load('./data/dims.log');
 k_box=2*pi/dims(2);
 dumx(1:2)=k_box;
-plot(dumx,dumy,'k-.')
-k_delta=4*pi/dims(1);
+plot(dumx,dumy,'b-.')
+k_delta=2*pi/dims(1);
 dumx(1:2)=k_delta;
-plot(dumx,dumy,'g--')
+plot(dumx,dumy,'b:')
 
 
 
